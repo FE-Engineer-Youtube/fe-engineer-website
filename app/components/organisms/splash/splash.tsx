a0ea135d90325048f22a82a6eb8b92d941d4b81f
Jonathan Hodak <10237065+jhodak@users.noreply.github.com>
2023-12-29 15:46:56 -0500

    fix: fixing splash

--- app/components/organisms/splash/splash.tsx
+++ app/components/organisms/splash/splash.tsx
@@ -1,16 +1,16 @@
-import { Stack, Text, Title } from "@mantine/core";
-import classes from "./splash.module.css";
+import { Stack, Text, Title } from '@mantine/core'
+import classes from './splash.module.css'
 
 const Splash = () => {
   return (
     <>
-      <Stack align="center" justify="center" p="lg" h={"100vh"}>
+      <Stack align="center" justify="center" p="lg" pt={64} pb={128}>
         <Title className={classes.title} ta="center">
           <Text
             inherit
             variant="gradient"
             component="span"
-            gradient={{ from: "green", to: "blue" }}
+            gradient={{ from: 'green', to: 'blue' }}
           >
             FE-Engineer.com
           </Text>
