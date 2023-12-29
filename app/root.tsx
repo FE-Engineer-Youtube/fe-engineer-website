3102475e5be2c55e82e9b7f260ea534d484efbff
Jonathan Hodak <10237065+jhodak@users.noreply.github.com>
2023-12-29 15:57:33 -0500

    fix: add favicons

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,7 @@
-import { ColorSchemeScript, MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css";
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import { ColorSchemeScript, MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css'
+import { cssBundleHref } from '@remix-run/css-bundle'
+import type { LinksFunction } from '@remix-run/node'
 import {
   Links,
   LiveReload,
@@ -9,10 +9,37 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+  ...(cssBundleHref
+    ? [
+        { rel: 'stylesheet', href: cssBundleHref },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: 'https://yt3.ggpht.com/s8Zhr1JBbxpqhHUhn6hmADJCQRgChgJFm7f3gzM28CFeC4IpZEEyeVvnBFlZUOsgk3ROoPFq5sc=s88-c-k-c0x00ffffff-no-rj',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: 'https://yt3.ggpht.com/s8Zhr1JBbxpqhHUhn6hmADJCQRgChgJFm7f3gzM28CFeC4IpZEEyeVvnBFlZUOsgk3ROoPFq5sc=s88-c-k-c0x00ffffff-no-rj',
+        },
+      ]
+    : [
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: 'https://yt3.ggpht.com/s8Zhr1JBbxpqhHUhn6hmADJCQRgChgJFm7f3gzM28CFeC4IpZEEyeVvnBFlZUOsgk3ROoPFq5sc=s88-c-k-c0x00ffffff-no-rj',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: 'https://yt3.ggpht.com/s8Zhr1JBbxpqhHUhn6hmADJCQRgChgJFm7f3gzM28CFeC4IpZEEyeVvnBFlZUOsgk3ROoPFq5sc=s88-c-k-c0x00ffffff-no-rj',
+        },
+      ]),
 ]
 
 export default function App() {
